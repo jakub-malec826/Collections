95bdb728ddc8cf301d0ec673d58f02975548663e
jakub-malec826 <jakub.malec826@gmail.com>
2022-10-18 23:30:02 +0200

    Changes to be committed: 	modified:   src/components/Comments.tsx

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,13 +1,16 @@
-import { Table } from "react-bootstrap";
+import { Button, Table } from "react-bootstrap";
 
 export default function Comments() {
-    return (
-        <Table striped bordered responsive>
-            <tbody>
-                <tr>
-                    <td>Likes: {}</td>
-                </tr>
-            </tbody>
-        </Table>
-    );
+	return (
+		<Table striped bordered responsive>
+			<tbody>
+				<tr>
+					<td>Likes: {}</td>
+					<td>
+						<Button variant="light">Like</Button>
+					</td>
+				</tr>
+			</tbody>
+		</Table>
+	);
 }
